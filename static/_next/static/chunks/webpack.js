ffdfd21402913d3737b2c479cbf1c4c49674ba66
Jose Torreblanca <josewhitetower@gmail.com>
2020-12-22 17:06:51 +0100

    updated static

--- static/_next/static/chunks/webpack.js
+++ static/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c489481ea829c6cdef31";
+/******/ 	var hotCurrentHash = "cbaf68fb5807399242f0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
